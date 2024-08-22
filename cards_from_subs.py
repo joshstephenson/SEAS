c23b3ff74cc058e72d9861898b579aed20606127
Joshua Stephenson <josh@brightmediums.com>
2024-08-21 19:35:11 -0500

    Resolving empty targets

--- cards_from_subs.py
+++ cards_from_subs.py
@@ -4,6 +4,7 @@ import argparse
 import os
 from datetime import datetime
 import re
+from nltk.tokenize import sent_tokenize
 
 TIMECODE_SEPARATOR      = ' --> '
 SENT_BOUNDARIES_REGEX   = r'[\!\.\?]$'
@@ -17,175 +18,17 @@ PARENTHESES_REGEX       = r'\(.*\)'
 LEADING_HYPHENS_REGEX   = r'^-'
 SRT_TIME_FORMAT         = '%H:%M:%S,%f'
 
-class Subtitles():
-    def __init__(self, text, sterilize=True, strip_captions=True, offset='00:00:00,000', offset_is_negative=False):
-        self.index = 0
-        self.subtitles = []
-        lines = text.split("\n\n")
-
-        # Best to only parse this once, rather than in the Subtitle class
-        offset = datetime.strptime(offset, SRT_TIME_FORMAT)
-
-        for line in lines:
-            sub_content = line.strip().split("\n")[1:]
-            if len(sub_content) == 0:
-                continue
-            subtitle = Subtitle(sub_content[0], sub_content[1:], offset, offset_is_negative)
-            if subtitle.has_content(sterilize, strip_captions):
-                self.subtitles.append(subtitle)
-
-        self.subtitles = self.merge_sentences()
-
-
-    def merge_sentences(self):
-        """
-        Merges subtitles into sentences. If a subtitle does not end with a word boundary,
-        the subsequent subtitles will be merged until a word boundary is found.
-        :return: merged subtitles
-        """
-        to_delete = []
-        current = None
-        found_boundary = True
-        for sub in self.subtitles:
-            if re.search(SENT_BOUNDARIES_REGEX, sub.text):
-                found_boundary = True
-            else:
-                # print(f'No word boundary for: {sub.text}')
-                found_boundary = False
-                if current is None:
-                    current = sub
-            if current is not None and current != sub:
-                current.merge(sub)
-                to_delete.append(sub)
-            if found_boundary:
-                current = None
-
-        return [s for s in self.subtitles if s not in to_delete]
-
-    def find_pairs(self, target):
-        pairs = []
-        for sub in self:
-            options = target.find(sub)
-            pairs.append(SubPair(sub, options))
-
-        if len(pairs) < 2:
-            return pairs
-
-        # Resolve overlaps
-        resolved = []
-        subsequent = pairs.pop(0)
-        while len(pairs) > 0:
-            current = subsequent
-            subsequent = pairs.pop(0)
-            commonality = current.commonality_with(subsequent)
-            if len(commonality) > 0:
-                for opt in commonality:
-                    with_current = current.source.overlap(opt)
-                    with_subsequent = subsequent.source.overlap(opt)
-                    if with_current > with_subsequent:
-                        subsequent.remove_option(opt)
-                    else:
-                        current.remove_option(opt)
-
-            resolved.append(current)
-        resolved.append(subsequent)
-
-        # Merge suboptions
-        for pair in resolved:
-            pair.merge_options()
-
-        return resolved
-
-    def find(self, other):
-        """
-        Find the counterpart to a subtitle provided from another language in the current set
-        :param other: a target subtitle to find the match
-        :return: counterpart subtitle or None
-        """
-
-        options = []
-        for current in self.subtitles:
-            if current.end < other.start:
-                continue
-            elif current.start > other.end:
-                break
-            else:
-                options.append(current)
-        return SubOptions(options)
-
-    def _find_best(self, sub, options):
-        # If more than one subtitle was found, select the one with the largest overlap
-        overlap = 0
-        best = None
-        for option in options:
-            _overlap = sub.overlap(option)
-            if _overlap > overlap:
-                overlap = _overlap
-                best = option
-        return best
-
-    def __iter__(self):
-        self.index = -1
-        self.current = self.subtitles[self.index]
-        return self
-
-    def __next__(self):
-        self.index += 1
-        if self.index < len(self.subtitles):
-            self.current = self.subtitles[self.index]
-            return self.current
-        else:
-            raise StopIteration
-
-class SubOptions():
-    def __init__(self, options:[]):
-        self.options = options
-        self.index = 0
-
-    def find_common(self, other):
-        common = []
-        for opt in self.options:
-            if opt in other.options:
-                common.append(opt)
-        return common
-
-    def merge(self):
-        merged = []
-        if len(self.options) > 1:
-            first = self.options[0]
-            for other in self.options[1:]:
-                first.merge(other)
-            merged.append(first)
-        elif len(self.options):
-            merged.append(self.options[0])
-        self.options = merged
-
-    def remove(self, item):
-        self.options.remove(item)
-
-    def __len__(self):
-        return len(self.options)
-
-    def __str__(self):
-        return "--".join(str(s) for s in self.options)
-
-    def __iter__(self):
-        self.index = -1
-        self.current = self.options[self.index]
-        return self
-
-    def __next__(self):
-        self.index += 1
-        if self.index < len(self.options):
-            self.current = self.options[self.index]
-            return self.current
-        else:
-            raise StopIteration
 
 class Subtitle():
     def __init__(self, timestring, text, offset, offset_is_negative):
+        self.offset = offset
+        self.offset_is_negative = offset_is_negative
+
         # join multiple lines of text into one line.
-        self.text = " ".join(text).strip()
+        if type(text) is not str:
+            self.text = " ".join(text).strip()
+        else:
+            self.text = text
 
         # Used to display subtitle timecodes
         self.timestring = timestring
@@ -285,10 +128,74 @@ class Subtitle():
     def __hash__(self):
         return hash(self.timestring)
 
+
+class SubOptions():
+    def __init__(self, options:[Subtitle]):
+        self.options = options
+        self.index = 0
+
+    def find_common(self, other):
+        common = []
+        for opt in self.options:
+            if opt in other.options:
+                common.append(opt)
+        return common
+
+    def merge(self):
+        merged = []
+        if len(self.options) > 1:
+            first = self.options[0]
+            for other in self.options[1:]:
+                first.merge(other)
+            merged.append(first)
+        elif len(self.options):
+            merged.append(self.options[0])
+        # merged should only have one entry
+        self.options = merged
+
+    def remove(self, item):
+        self.options.remove(item)
+
+    def sentences(self) -> [str]:
+        text = " ".join(str(s) for s in self.options)
+        sentences = sent_tokenize(text)
+        return sentences
+
+    def remove_last_sentence(self) -> Subtitle:
+        opt: Subtitle = self.options[-1]
+        sentence = self.sentences()[-1]
+        opt.text = opt.text.replace(sentence, '')
+        sub = Subtitle(opt.timestring, sentence, opt.offset, opt.offset_is_negative)
+        return sub
+
+    def add_option(self, option:Subtitle):
+        self.options.append(option)
+
+    def __len__(self):
+        return len(self.options)
+
+    def __str__(self):
+        return "--".join(str(s) for s in self.options)
+
+    def __iter__(self):
+        self.index = -1
+        self.current = self.options[self.index]
+        return self
+
+    def __next__(self):
+        self.index += 1
+        if self.index < len(self.options):
+            self.current = self.options[self.index]
+            return self.current
+        else:
+            raise StopIteration
+
+
 class SubPair():
     def __init__(self, source:Subtitle, sub_options:SubOptions):
         self.source = source
         self.sub_options = sub_options
+        self.flagged_for_delete = False
 
     def commonality_with(self, other):
         return self.sub_options.find_common(other.sub_options)
@@ -299,14 +206,182 @@ class SubPair():
     def merge_options(self):
         self.sub_options.merge()
 
+    def has_no_target(self):
+        return len(self.sub_options) == 0
+
+    def target_sentences(self):
+        sentences = self.sub_options.sentences()
+        return [s for s in sentences if len(s)]
+
+    def pop_last_sentence_as_subtitle(self) -> Subtitle:
+        sentence = self.sub_options.remove_last_sentence()
+        return sentence
+
+    def add_option(self, option:Subtitle):
+        self.sub_options.add_option(option)
+
+    def add_sentence_to_source(self, sentence:str):
+        self.source.text = self.source.text + ' ' + sentence
+
+    def resolve_with(self, last, subsequent, sterilize=True, strip_captions=True):
+        # Resolve situations where two subsequent source subtitles have overlap with the same target subtitle
+        # by assigning the target sub to the source with the greatest overlap
+        commonality = self.commonality_with(subsequent)
+        print(subsequent.source)
+        if len(commonality) > 0:
+            for opt in commonality:
+                with_current = self.source.overlap(opt)
+                with_subsequent = subsequent.source.overlap(opt)
+                if with_current > with_subsequent:
+                    subsequent.remove_option(opt)
+                    # Now we have to check that it's not empty
+                    # print(f'HERE ({subsequent.source}): {.sub_options}')
+                    if subsequent.has_no_target():
+                        print('now other has no target')
+                else:
+                    self.remove_option(opt)
+
+            # Sometimes a subtitle has no targets, most likely because the previous subtitle "swallowed" its target
+            # So we should merge the sources
+            if subsequent.has_no_target():
+                # If the previous subpair has more than one sentence, move the last sentence to subsequent
+                if len(self.target_sentences()) > 1:
+                    sub = self.pop_last_sentence_as_subtitle()
+                    if sub.has_content(sterilize, strip_captions):
+                        subsequent.add_option(sub)
+                # If the previous subpair does not have more than one sentence, move subsequent subtitle source
+                # to current source
+                else:
+                    self.add_sentence_to_source(subsequent.source.text)
+                    subsequent.flagged_for_delete = True
+
     def __str__(self):
-        if len(self.sub_options.options) > 1:
-            print(self.source)
-            print('--')
-            for sub in self.sub_options.options:
-                print(sub)
         return f'- {self.source}\n- {self.sub_options}\n'
 
+
+class Subtitles():
+    def __init__(self, text, sterilize=True, strip_captions=True, offset='00:00:00,000', offset_is_negative=False):
+        self.index = 0
+        self.subtitles = []
+        lines = text.split("\n\n")
+
+        # Best to only parse this once, rather than in the Subtitle class
+        offset = datetime.strptime(offset, SRT_TIME_FORMAT)
+
+        for line in lines:
+            sub_content = line.strip().split("\n")[1:]
+            if len(sub_content) == 0:
+                continue
+            subtitle = Subtitle(sub_content[0], sub_content[1:], offset, offset_is_negative)
+            if subtitle.has_content(sterilize, strip_captions):
+                self.subtitles.append(subtitle)
+
+        self.subtitles = self.merge_sentences()
+
+
+    def merge_sentences(self):
+        """
+        Merges subtitles into sentences. If a subtitle does not end with a word boundary,
+        the subsequent subtitles will be merged until a word boundary is found.
+        :return: merged subtitles
+        """
+        to_delete = []
+        current = None
+        found_boundary = True
+        for sub in self.subtitles:
+            if re.search(SENT_BOUNDARIES_REGEX, sub.text):
+                found_boundary = True
+            else:
+                # print(f'No word boundary for: {sub.text}')
+                found_boundary = False
+                if current is None:
+                    current = sub
+            if current is not None and current != sub:
+                current.merge(sub)
+                to_delete.append(sub)
+            if found_boundary:
+                current = None
+
+        return [s for s in self.subtitles if s not in to_delete]
+
+    def align(self, target):
+        """
+        Aligns the subtitles in target by iterating over the subtitles in self and looking for a match in target
+        based on timecodes. The targets found might be multiple in which case the SubPair object will have one source
+        and multiple sub_options. Those will be resolved in this function as well (if possible) by selecting the
+        options with the greatest overlap, compared to the previous subtitle.
+        :param target: Another Subtitles object for corresponding language
+        :return: a list of SubPair objects
+        """
+        pairs = []
+        for sub in self:
+            options = target.find(sub)
+            pairs.append(SubPair(sub, options))
+
+        if len(pairs) < 2:
+            return pairs
+
+        # Resolve overlaps
+        resolved = []
+        last = None
+        current = None
+        subsequent = pairs.pop(0)
+        while len(pairs) > 0:
+            last = current
+            current = subsequent
+            subsequent = pairs.pop(0)
+            current.resolve_with(last, subsequent)
+            if not current.flagged_for_delete:
+                resolved.append(current)
+        if not subsequent.flagged_for_delete:
+            resolved.append(subsequent)
+
+        # Merge suboptions
+        for pair in resolved:
+            pair.merge_options()
+
+        return resolved
+
+    def find(self, other):
+        """
+        Find the counterpart to a subtitle provided from another language in the current set
+        :param other: a target subtitle to find the match
+        :return: counterpart subtitle or None
+        """
+
+        options = []
+        for current in self.subtitles:
+            if current.end < other.start:
+                continue
+            elif current.start > other.end:
+                break
+            else:
+                options.append(current)
+        return SubOptions(options)
+
+    def _find_best(self, sub, options):
+        # If more than one subtitle was found, select the one with the largest overlap
+        overlap = 0
+        best = None
+        for option in options:
+            _overlap = sub.overlap(option)
+            if _overlap > overlap:
+                overlap = _overlap
+                best = option
+        return best
+
+    def __iter__(self):
+        self.index = -1
+        self.current = self.subtitles[self.index]
+        return self
+
+    def __next__(self):
+        self.index += 1
+        if self.index < len(self.subtitles):
+            self.current = self.subtitles[self.index]
+            return self.current
+        else:
+            raise StopIteration
 
 def main(opts):
     source = os.path.expanduser(opts.source)
@@ -323,7 +398,7 @@ def main(opts):
     source_subs = Subtitles(stext, opts.sterilize, opts.strip_captions)
     target_subs = Subtitles(ttext, opts.sterilize, opts.strip_captions, opts.offset, opts.offset_is_negative)
 
-    pairs = source_subs.find_pairs(target_subs)
+    pairs = source_subs.align(target_subs)
     for pair in pairs:
         print(pair)
 
