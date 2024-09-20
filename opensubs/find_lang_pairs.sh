865147f5bb42791ecfb2f3d1365ddbd3a3d54e8f
Joshua Stephenson <josh@brightmediums.com>
2024-09-20 16:21:35 -0500

    bash script to get language support from sqlite file

---
+++ opensubs/find_lang_pairs.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+
+rm -f lang_support.txt
+LANGS=(eng spa fre dut ita ger swe kor chi jpn)
+for LG in ${LANGS[@]}; do
+    echo -n "$LG: " >> lang_support.txt
+    sqlite3 subtitles.db "select count(*) as combo from (select distinct title from subtitles where lang in ('$LG'));" >> lang_support.txt
+done
+
+LANGS=(fre dut ita ger swe kor chi jpn)
+for LG in ${LANGS[@]}; do
+    echo -n "eng, spa, $LG: " >> lang_support.txt
+    sqlite3 subtitles.db "select count(*) as combo from (select title from subtitles where lang in ('eng', 'spa', '$LG') group by title having count(distinct lang) = 3);" >> lang_support.txt
+done
+
+LANGS=(dut ita ger swe kor chi jpn)
+for LG in ${LANGS[@]}; do
+    echo -n "eng, spa, fre, $LG: " >> lang_support.txt
+    sqlite3 subtitles.db "select count(*) as combo from (select title from subtitles where lang in ('eng', 'spa', 'fre', '$LG') group by title having count(distinct lang) = 4);" >> lang_support.txt
+done
