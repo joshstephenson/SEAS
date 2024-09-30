c2d205e5cdc5bb60cdcfdcd85b0b50a95d7f8ea3
Joshua Stephenson <josh@brightmediums.com>
2024-09-29 19:47:29 -0500

    Create count_by_year.sh
    uses environment var for SUBTITLE_DATA (location of opensubtitles)

---
+++ scripts/count_by_year.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+YEAR=2000
+echo "YEAR, FILES, TITLES"
+while [[ $YEAR -lt 2025 ]];do
+    COUNT=$(sqlite3 "$SUBTITLE_DATA/subtitles.db" "select count(*) from subtitles where year='$YEAR';")
+    UNIQ=$(sqlite3 "$SUBTITLE_DATA/subtitles.db" "select count(*) as combo from (select distinct title from subtitles where year='$YEAR');")
+    echo "$YEAR, $COUNT, $UNIQ"
+    YEAR=$(($YEAR+1))
+done
