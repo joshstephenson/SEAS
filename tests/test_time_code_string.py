e61cdd463c01547be76ae890bc8d0212277e8fef
Joshua Stephenson <josh@brightmediums.com>
2024-09-29 19:47:51 -0500

    Added test for various timecode string separators

---
+++ tests/test_time_code_string.py
@@ -0,0 +1,14 @@
+import pytest
+from subtitles import Subtitles
+from subtitle import Subtitle
+from align import get_text
+
+@pytest.fixture
+def subs():
+    text = get_text('test_data/timecode_strings.srt')
+    return Subtitles(text)
+
+def test_timecode_string_can_be_parsed(subs):
+    for sub in subs:
+        assert sub.start is not 0 # 0 is default
+        assert sub.end is not 0   # 0 is default
