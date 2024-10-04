e2b42d53e36b8d858003d3307a5451ea41808222
Joshua Stephenson <josh@brightmediums.com>
2024-10-04 08:42:19 -0500

    uber_script now takes path to title and 2 languages as arguments

--- scripts/uber_script.sh
+++ scripts/uber_script.sh
@@ -3,18 +3,23 @@
 if [ -z "$SUBTITLE_REPO" ]; then
     echo "Please set SUBTITLE_REPO environment variable to the root of this repository."
 fi
-if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "Usage: $0 [source] [target]"
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] ; then
+    echo "Usage: $0 [title dir] [source lang code] [target lang code]"
+    exit 0
 fi
 
-SOURCE="$1"
-TARGET="$2"
+BASE_DIR="$1"
+SOURCE_LANG="$2"
+TARGET_LANG="$3"
+
+# First we have to find subtitle files to use
+SOURCE=$(find "$BASE_DIR/$SOURCE_LANG" -iname "*.srt" -exec ls -S {} \; | tail -n 1)
+TARGET=$(find "$BASE_DIR/$TARGET_LANG" -iname "*.srt" -exec ls -S {} \; | tail -n 1)
+echo "SOURCE: $SOURCE"
+echo "TARGET: $TARGET"
 
 SOURCE_SENT="${SOURCE/.srt/.sent}"
 TARGET_SENT="${TARGET/.srt/.sent}"
-BASE_DIR="$(dirname $(dirname $SOURCE))"
-SOURCE_LANG="$(basename $(dirname $SOURCE))"
-TARGET_LANG="$(basename $(dirname $TARGET))"
 TIME_FILE="$BASE_DIR/${SOURCE_LANG}-${TARGET_LANG}-timecode.txt"
 VEC_ALIGN_FILE="$BASE_DIR/${SOURCE_LANG}-${TARGET_LANG}-vec.align"
 VEC_FILE="$BASE_DIR/${SOURCE_LANG}-${TARGET_LANG}-vec.txt"
@@ -24,20 +29,25 @@ GOLD_FILE="$BASE_DIR/${SOURCE_LANG}-${TARGET_LANG}.gold"
 if [ ! -f "$TIME_FILE" ]; then
     $SUBTITLE_REPO/scripts/align.py -s "$SOURCE" -t "$TARGET" > "$TIME_FILE"
 fi
+echo "$TIME_FILE"
 #
 # That was fast, now do the embedding method
 if [ ! -f "$SOURCE_SENT" ]; then
     $SUBTITLE_REPO/scripts/srt2sent.py -f "$SOURCE" > "$SOURCE_SENT"
 fi
+echo "$SOURCE_SENT"
 if [ ! -f "$TARGET_SENT" ]; then
     $SUBTITLE_REPO/scripts/srt2sent.py -f "$TARGET" > "$TARGET_SENT"
 fi
+echo "$TARGET_SENT"
 if [ ! -f "$VEC_ALIGN_FILE" ]; then
-    $SUBTITLE_REPO/scripts/sent2emb.sh "$SOURCE_SENT" "$TARGET_SENT" | grep -v "| INFO |" > "$VEC_ALIGN_FILE"
+    $SUBTITLE_REPO/scripts/sent2emb.sh "$SOURCE_SENT" "$TARGET_SENT" | grep -v "| INFO |" > "$VEC_ALIGN_FILE" 2>/dev/null
 fi
+echo "$VEC_ALIGN_FILE"
 if [ ! -f "$VEC_FILE" ]; then
     $SUBTITLE_REPO/scripts/emb2align.py -s "$SOURCE_SENT" -t "$TARGET_SENT" -a "$VEC_ALIGN_FILE" > "$VEC_FILE"
 fi
+echo "$VEC_FILE"
 $SUBTITLE_REPO/scripts/results_analyzer.py -f "$TIME_FILE" "$VEC_FILE" -o "$GOLD_FILE" -a
 
 
