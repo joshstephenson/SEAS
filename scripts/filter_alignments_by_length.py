abd9bde3727923ab80bf3e7f851e48b19bb93d96
Joshua Stephenson <josh@brightmediums.com>
2024-11-03 10:34:05 -0500

    Update filter_alignments_by_length.py
    ensure script exits

--- scripts/filter_alignments_by_length.py
+++ scripts/filter_alignments_by_length.py
@@ -2,24 +2,31 @@
 
 import argparse
 import sys
+from itertools import islice
 
 import regex
 
 
 def main(opts):
-    with open(opts.file, 'r', encoding='latin-1') as source_file:
-        # read 2 lines
-        while True:
-            lines = [source_file.readline().strip() for _ in range(2)]
-            if len(lines[0]) > opts.min_length and len(lines[1]) > opts.min_length:
-                sys.stdout.write('\n'.join(lines) + '\n\n')
+    file = sys.stdin if opts.file is None else open(opts.file, 'r', encoding='latin-1')
+    for lines in iter(lambda: list(islice(file, 3)), []):
+        # Read 2 lines
 
-            empty = source_file.readline().strip()
-            assert len(empty) == 0
+        if len(lines[0]) >= opts.min_length and len(lines[1]) >= opts.min_length:
+            if opts.max_length is not None:
+                if len(lines[0]) <= opts.max_length and len(lines[1]) <= opts.max_length:
+                    sys.stdout.write(''.join(lines))
+            else:
+                sys.stdout.write(''.join(lines))
+
+        # Then read an empty line
+        assert len(lines[2].strip()) == 0
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--file', required=True)
-    parser.add_argument('-l', '--min-length', default=30, type=int)
+    parser.add_argument('-f', '--file', required=False)
+    parser.add_argument('-min', '--min-length', default=30, type=int)
+    parser.add_argument('-max', '--max-length', required=False, type=int)
     args = parser.parse_args()
     main(args)
